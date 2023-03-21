e83acc7f2ca6050eef192e89c76473e489ad7f53
Byron2016 <bgva2005@yahoo.com>
2023-03-21 18:54:42 -0500

    feat: cvs incluir main.mjs con vite

---
+++ student-cvs/Byron-8971/vite.config.js
@@ -0,0 +1,15 @@
+import { defineConfig, loadEnv } from "vite";
+import path from "path";
+
+export default {
+  root: path.resolve(__dirname, "src"),
+  build: {
+    outDir: path.resolve(__dirname, "dist"),
+    rollupOptions: {
+      input: {
+        index: path.resolve(__dirname, "src/index.html"),
+        about: path.resolve(__dirname, "src/about.html"),
+      },
+    },
+  },
+};
