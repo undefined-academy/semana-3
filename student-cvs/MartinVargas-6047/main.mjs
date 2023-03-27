c35c145603e3d271b7f95aa3cea559a7101d47ff
Martin <martinrodrigo.vargas93@gmail.com>
2023-03-27 09:17:57 -0300

    [Semana3, Clase4] Moviendo archivos - añadiendo mjs

---
+++ student-cvs/MartinVargas-6047/main.mjs
@@ -0,0 +1,6 @@
+function handleSubmit(event) {
+  event.preventDefault(); // 🍎
+  console.log("Hola mundo");
+}
+const $form = document.querySelector("form");
+$form.addEventListener("submit", handleSubmit);
