3a3a727ae1db99b883ba88da6c9d74f409391849
Aeternus185 <andryp_@hotmail.com>
2023-03-28 02:44:20 -0400

    build: 🔧 Setup Tailwind CSS Config

---
+++ student-forms/AETERNUS-9050/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['*'],
+	theme: {
+		fontFamily: {
+			sans: ['Montserrat', 'sans-serif'],
+		},
+		extend: {},
+	},
+	plugins: [],
+};
