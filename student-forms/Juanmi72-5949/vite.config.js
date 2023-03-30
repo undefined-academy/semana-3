9f622a237668058256d2186e71814bfd217cdefa
Juanmi72 <juanmifer@gmail.com>
2023-03-31 00:19:58 +0200

    Solucionado problema con el path de Vite

--- student-forms/Juanmi72-5949/vite.config.js
+++ student-forms/Juanmi72-5949/vite.config.js
@@ -1,7 +1,13 @@
-const path = require('path')
+/*const path = require('path')*/
+import path from 'path';
 
 export default {
   root: path.resolve(__dirname, 'src'),
+  resolve: {
+    alias: {
+      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+    }
+  },
   server: {
     port: 8080,
     hot: true
