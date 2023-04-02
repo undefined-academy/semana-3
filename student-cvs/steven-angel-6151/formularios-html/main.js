a439557b32ddba80e22f973f619984742488ecfe
Steven Angel <stevenangel.ing@gmail.com>
2023-04-02 15:27:17 -0500

    Archivo main.js agregado al formulario

---
+++ student-cvs/steven-angel-6151/formularios-html/main.js
@@ -0,0 +1,14 @@
+
+
+function handleSubmit(event) {
+
+    event.preventDefault(); //
+    console.log("Hola mundo");
+   }
+
+
+const $form =
+   document.querySelector("form");
+   $form.addEventListener("submit",
+   handleSubmit);
+   
