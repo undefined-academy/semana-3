272eea76c4bf654d71f23869def4ec347f90d55c
Joshep <joshepvillalba@hotmail.com>
2023-04-02 12:57:45 -0600

    Formulario con bootstrap

---
+++ student-forms/croxz-2619/src/js/form.js
@@ -0,0 +1,8 @@
+// prevent default
+
+const form = document.querySelector('#form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  console.log('Formulario enviado!');
+});
