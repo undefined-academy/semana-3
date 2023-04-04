4d80304e32b9ad9eae2b2d6432153170fca24529
David <imtcdavidochoa@gmail.com>
2023-04-03 22:10:58 -0700

    Se agrego archivo js main.mjs

---
+++ student-cvs/DavidOe-6229/main.mjs
@@ -0,0 +1,10 @@
+
+function handleSubmit (event) {
+    event.preventDefault ()
+    console.log("Hola Mundo");
+}
+
+const $form = document.querySelector("form")
+
+$form.addEventListener("submit", handleSubmit)
+
