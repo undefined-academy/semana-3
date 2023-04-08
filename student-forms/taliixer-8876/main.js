eb3a589cc14d97decb5c02e45b424cfda808e1b3
Bryan Vera <brayanandresveragar@gmail.com>
2023-04-07 22:40:24 -0500

    :heavy_minus_sign: Limpie la mesa de trabajo

--- student-forms/taliixer-8876/main.js
+++
@@ -1,7 +0,0 @@
-function handleSubmit(event) {
-    event.preventDefault(); // 🍎
-    console.log("Hola mundo");
-}
-    
-const $form =document.querySelector("form");
-$form.addEventListener("submit", handleSubmit);
