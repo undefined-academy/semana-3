b86b816fe3dd9266b77c43ee0bc87659ed6506d9
joelhuba <joelhuba1999@gmail.com>
2023-04-08 08:33:10 -0500

    ejercicios-semana-3

---
+++ student-forms/joelhube-5339/bootstrap/src/js/main.js
@@ -0,0 +1,7 @@
+import '../scss/styles.scss'
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+
+import Alert from 'bootstrap/js/dist/alert';
+import { Tooltip, Toast, Popover } from 'bootstrap';
