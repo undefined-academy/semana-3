348c55864a0a35f38a62c16d81bee1b83232f8f8
Jeremy Orellana jejorm8@gmail.com <jejorm8@gmail.com>
2023-04-09 15:52:56 -0500

    Cv profile using mobile first

---
+++ student-cvs/JeremyO-7130/main.mjs
@@ -0,0 +1,7 @@
+function hanldeSubmit(event) {
+  event.preventDefault()
+  console.log('Hola mundo')
+}
+
+const $form = document.querySelector('form')
+$form.addEventListener("submit", hanldeSubmit)
