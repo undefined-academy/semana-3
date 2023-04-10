0fe53135e7000173cac8224f5944abd12bf4b52b
Angel <angelmijarees@gmail.com>
2023-04-10 17:44:31 -0400

    Cambios

---
+++ student-forms/Angel-Mijares-3276/main.mjs
@@ -0,0 +1,8 @@
+function handleSumit(event) {
+    event.preventDefault()
+    console.log("Hola Mundo");
+}
+
+const $form = document.querySelector("form");
+
+$form.addEventListener("submit", handleSumit)
