aa74dcd8f66226f374b3bb8b935af9d764497db6
Marcelo Reyna Rangel <marcelo.reyran@gmail.com>
2023-04-11 17:14:32 -0600

    Delete student-cvs/Marcelo Reyna-8629 directory

--- student-cvs/Marcelo Reyna-8629/main.mjs
+++
@@ -1,7 +0,0 @@
-function handleSubmit (event){
-    event.preventDefault();
-    console.log("hello world");
-    }
-    
-    const $form = document.querySelector("form");
-    $form.addEventListener("submit",handleSubmit );
