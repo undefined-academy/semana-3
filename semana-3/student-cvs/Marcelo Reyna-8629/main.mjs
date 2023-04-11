a0f784dbaca95ed84e7a0f0b1efbdd94c5886dd6
Marcelo Reyna Rangel <marcelo.reyran@gmail.com>
2023-04-11 17:21:26 -0600

    Semana-3

---
+++ semana-3/student-cvs/Marcelo Reyna-8629/main.mjs
@@ -0,0 +1,7 @@
+function handleSubmit (event){
+    event.preventDefault();
+    console.log("hello world");
+    }
+    
+    const $form = document.querySelector("form");
+    $form.addEventListener("submit",handleSubmit );
