454347ff18bab1ca9b4aea8631e6dcfdc0bd8df0
hardstonepaul <hardstonepaul@hotmail.com>
2023-04-11 00:36:57 -0500

    Style form with tailwindcss

--- student-forms/hardstone-3508/tailwind.config.js
+++ student-forms/hardstone-3508/tailwind.config.js
@@ -1,8 +1,28 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+
+const withMT = require("@material-tailwind/html/utils/withMT");
+module.exports = withMT({
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        myblue: {
+          50: "#eaf1fb",
+          100: "#c0d5f2",
+          200: "#96bae9",
+          300: "#6c9ee0",
+          400: "#4182d7",
+          500: "#2869be",
+          600: "#1f5193",
+          700: "#163a69",
+          800: "#0d233f",
+          900: "#040c15",
+        }
+      },
+      fontFamily: {
+        space: ["Space Grotesk", "sans-serif"],
+      }
+    },
   },
   plugins: [],
-}
+});
