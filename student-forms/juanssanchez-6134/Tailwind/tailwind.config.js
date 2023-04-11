e2a5c902c6d4aa51713768b7632ed1cb7d4528eb
Juan <js.sanchez130@gmail.com>
2023-04-10 23:45:02 -0500

    Agregar Formulario de contacto hecho con Tailwind

---
+++ student-forms/juanssanchez-6134/Tailwind/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      darkergreen: "#0f380f",
+      darkgreen: "#306230",
+      green: "#8bac0f",
+      lightgreen: "#9bbc0f",
+      gray: "#aaaaaa",
+      magenta: "#9a2257",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
