3e9d98cc35c059e30be1e63651386f1a0b92df7a
Pancratzia <le.ortegaperez@gmail.com>
2023-04-17 19:02:08 -0400

    Asignación de la clase 3 lista

--- student-cvs/Pancratzia-4189/assets/js/scripts.js
+++ student-cvs/Pancratzia-4189/assets/js/scripts.js
@@ -1,5 +1,7 @@
-const form = document.querySelector("form");
-
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-});
+function handleSubmit(event) {
+  event.preventDefault(); //Evita que se recargue la página
+  console.log("Hola mundo");
+}
+  
+const $form =document.querySelector("form");
+$form.addEventListener("submit", handleSubmit);
