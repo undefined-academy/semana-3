78f980bcee0aa87c2a9da3354ee43c8fdb9cfd9f
jesab3l <jesabelmamani@gmail.com>
2023-04-19 15:44:24 -0400

    Se agrega required y cambios en el js para el submit del formulario

--- student-forms/Jessbel-7277/Bootstrap/src/js/main.js
+++ student-forms/Jessbel-7277/Bootstrap/src/js/main.js
@@ -3,3 +3,11 @@ import '../scss/styles.scss'
 
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
+
+function handleSubmit(event){
+    event.preventDefault();
+}
+
+const $form = document.querySelector("form");
+
+$form.addEventListener("submit", handleSubmit);
