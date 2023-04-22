824ea041f862e952b9bcb8d435cb2255c89a3205
Eduardo <ejrb1234@gmail.com>
2023-04-22 15:41:34 -0400

    Acá agregué el archivo package.json los scripts de vite

--- student-cvs/MrRedu-7518/form-contacto_js.js
+++ student-cvs/MrRedu-7518/form-contacto_js.js
@@ -1,3 +1,5 @@
+// Esta fue la configuración para prevenir el comportamiento por default del envio del formulario que yo encontré en internet, la que tiene extensión de .mjs, es la que se agrega en el ejercicio de la Semana3-Clase3
+
 document.querySelector('form').addEventListener('submit', function(event) {
     event.preventDefault(); 
     // Evita el comportamiento de envío por defecto
