3e8978f8eaaea5785789ee4ade67f51c627ba6d2
OrcaCL <orlando.caffarena@gmail.com>
2023-04-28 01:14:24 -0400

    abortado tailwind, solo bootstrap

--- student-forms/Caffa-4633/tailwind/src/main.js
+++
@@ -1,9 +0,0 @@
-//Agarrar el submit del form.
-function handleSubmit(event) {
-    event.preventDefault(); //🍎
-    console.log("Hola Mundo: Manejado");
-}
-
-//El $ es una convención o estándar en donde no es solo un trozo de HTML, sino todo el objeto del DOM.
-const $form = document.querySelector("form");
-$form.addEventListener("submit", handleSubmit);
