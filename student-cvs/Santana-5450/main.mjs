ec5374bc1118a32437ee8e9060ad45d2d53cfbd4
Santana <warsant@hotmail.com>
2023-05-01 16:51:30 -0400

    Ejercicios semana 3, Flexbox, Css Grid, Vite

---
+++ student-cvs/Santana-5450/main.mjs
@@ -0,0 +1,9 @@
+//main.mjs
+function handleSubmit(event){
+    event.preventDefault();
+    console.log("Hola Mundo");
+}
+
+const $form =
+document.querySelector("form");
+$form.addEventListener("submit", handleSubmit);
