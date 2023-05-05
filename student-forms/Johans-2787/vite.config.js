887e6a080110b70d6108cc436c8ecb5bcb084b34
Snahoj <akjohans@gmail.com>
2023-05-06 01:55:13 +0200

    change requested

--- student-forms/Johans-2787/vite.config.js
+++ student-forms/Johans-2787/vite.config.js
@@ -2,11 +2,6 @@ const path = require('path')
 
 export default {
   root: path.resolve(__dirname, 'src'),
-  resolve: {
-    alias: {
-        '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
-    }
-  },
   build: {
     outDir: '../dist'
   },
