b4f36071a4d15f3a19237cade43bfe63cf989a30
jayler castillo <jayler0203@gmail.com>
2023-05-05 20:58:26 -0500

    eliminando js inecesario

--- student-forms/jayler-1407/src/js/main.js
+++ student-forms/jayler-1407/src/js/main.js
@@ -4,14 +4,6 @@ import '../scss/styles.scss'
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
 
-// main.mjs
-function handleSubmit(event) {
-    event.preventDefault(); // 🍎
-    console.log("Hola mundo");
-}
-    
-const $form =document.querySelector("form");
-$form.addEventListener("submit", handleSubmit);
 
 const $slider = document.querySelector(".slider")
 console.log($slider.value)
