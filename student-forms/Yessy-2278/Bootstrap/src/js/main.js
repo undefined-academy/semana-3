7a182fb8b32ff028718a0e23ecdba062e9688e56
YessyC <yessicasta@gmail.com>
2023-05-16 20:06:07 -0500

    se agrega required a inputs del form, se agrega el JS para verificación de envío del form, formato camelCase en etiqueta sugerida, adición de scripts en el package.json. sugerencias de MrRedu

--- student-forms/Yessy-2278/Bootstrap/src/js/main.js
+++ student-forms/Yessy-2278/Bootstrap/src/js/main.js
@@ -1,5 +1,12 @@
 // Import our custom CSS
 import '../scss/styles.scss'
+function handleSubmit(event) {
+    event.preventDefault(); // 🍎
+    console.log("Hola mundo");
+}
+    
+const $form =document.querySelector("form");
+$form.addEventListener("submit", handleSubmit);
 
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap'
