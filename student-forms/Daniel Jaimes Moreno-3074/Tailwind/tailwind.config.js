82161ff0b7e01a959b605dc3984557628ab16594
DanielJaimes <d.ricardo.j@gmail.com>
2023-06-23 14:49:36 -0600

    add form

---
+++ student-forms/Daniel Jaimes Moreno-3074/Tailwind/tailwind.config.js
@@ -0,0 +1,12 @@
+
+// Older version of tailwind.config.js file
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  }
+  
