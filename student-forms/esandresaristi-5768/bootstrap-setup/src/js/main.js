4a85e91dca887fdfb983e55e4c5638f6001353bf
andresarioro <andresarioro@gmail.com>
2023-12-04 20:01:02 -0500

    Agregando formulario con bootstrap

---
+++ student-forms/esandresaristi-5768/bootstrap-setup/src/js/main.js
@@ -0,0 +1,26 @@
+// Import our custom CSS
+import '../scss/styles.scss'
+
+// Import all of Bootstrap's JS
+
+import * as bootstrap from 'bootstrap'
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(() => {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+      form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+  
+        form.classList.add('was-validated')
+      }, false)
+    })
+  })()
